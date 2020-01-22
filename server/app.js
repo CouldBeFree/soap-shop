9b1d239d8dabfc6556a5e3ef6ee9f2f59f68e224
couldbefree <propokerstar90@gmail.com>
2020-01-23 01:31:05 +0200

    created model product

--- server/app.js
+++ server/app.js
@@ -1,5 +1,29 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const morgan = require('morgan');
+
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
+
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect('mongodb://localhost:27017/soap', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected')
+});
 
 const port = 5000;
 app.listen(port, () => {
--- server/models/product.js
+++ server/models/product.js
@@ -2,7 +2,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const mongoosePaginate = require('mongoose-paginate');
 
-
+const ModelSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    unique: true
+  },
+  category: {
+    type: String,
+    required: [true, 'Category is required'],
+    enum: [
+      'woman',
+      'man',
+      'baby-soap',
+      'bouquets',
+      'kits'
+    ]
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+  },
+  thumb: {
+    type: Buffer,
+    contentType: String
+  }
+});
 
 ModelSchema.plugin(mongoosePaginate);
 
