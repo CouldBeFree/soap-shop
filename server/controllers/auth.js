22d10d721b2dfb01fadd80d8a9edc344dd565fcb
couldbefree <propokerstar90@gmail.com>
2020-03-03 11:11:37 +0200

    forgot/reset password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,8 @@
 const User = require('../models/user');
 const asyncHandler = require('../middleware/async');
 const errorResponse = require('../utils/errorResponse');
+const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
 
 // @desc Register new user
 // @route POST api/v1/register/
@@ -101,10 +103,8 @@ exports.updatePassword = asyncHandler(async (req, res, next) => {
 
 // @desc    Forgot password
 // @route   PUT /api/v1/auth/forgot-password
-// @access  Private
+// @access  Public
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
-  console.log('email', req.body.email);
-  const { email } = req.body.email;
 
   const user = await User.findOne({ $and: [ {method: 'local'}, { "local.email": req.body.email } ] });
 
@@ -114,7 +114,60 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   }
 
   // Get reset token
-  const resetToken = user.getResetPasswordToken();
+  const resetToken = await user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/reset-password/${resetToken}`;
+
+  const message = `Please make PUT request to: \n\n ${resetUrl}`;
+
+  try{
+    await sendEmail({
+      email: user.local.email,
+      subject: 'Password reset token',
+      message
+    });
+
+    res.status(200).json({ success: true, data: 'Email sent' })
+  } catch (err) {
+    console.log(err);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new errorResponse('Email could not be sent', 500))
+  }
+
+  sendTokenResponse(user, 200, res);
+});
+
+// @desc    Reset password
+// @route   PUT /api/v1/auth/reset-password/:resettoken
+// @access  Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+  // Get hashed token
+  const resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(req.params.resettoken)
+    .digest('hex');
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() }
+  });
+
+  if(!user){
+    return next(new errorResponse('Invalid token', 400))
+  }
+
+  // Set new password
+  user.local.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+  await user.save();
 
   sendTokenResponse(user, 200, res);
 });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const { registerUser, login, getMe, updateDetails, updatePassword, googleOAuthLogin, facebookLogin, forgotPassword } = require('../controllers/auth');
+const {
+  registerUser,
+  login,
+  getMe,
+  updateDetails,
+  updatePassword,
+  googleOAuthLogin,
+  facebookLogin,
+  forgotPassword,
+  resetPassword
+} = require('../controllers/auth');
 const passportConf = require('../config/passport');
 const passportSignIn = passport.authenticate('local', { session: false });
 const passportJWT = passport.authenticate('jwt', { session: false });
@@ -19,6 +29,8 @@ router.put('/update-details', passportJWT, updateDetails);
 router.put('/update-password', passportJWT, updatePassword);
 
 router.post('/forgot-password', forgotPassword);
+
+router.put('/reset-password/:resettoken', resetPassword);
 
 router.post('/google', passportGoogleOAuth, googleOAuthLogin);
 
